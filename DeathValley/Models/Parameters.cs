9c9ddd17b4787f28635320c7110ffc09a54a001d
Unknown <nevermor9696@gmail.com>
2018-03-30 11:36:16 +0300

    Create project

---
+++ DeathValley/Models/Parameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeathValley.Models
+{
+    public class Parameters
+    {
+        int Parameter1 { get; set; }
+        int Parameter2 { get; set; }
+        int Parameter3 { get; set; }
+        int Step { get; set; }
+        int Range1 { get; set; }
+        int Range2 { get; set; }
+    }
+}
